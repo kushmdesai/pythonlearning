fbb5555f582c943f397e72d83ce256f2f4d9a374
kushmdesai <kushmdesai@icloud.com>
2022-12-26 11:43:41 -0600

    made a change so the output is easier to read

--- adding.py
+++ adding.py
@@ -2,4 +2,5 @@ from funclib import addingnum
 numa1 = int(input("Enter number 1: "))    
 numa2 = int(input("Enter number 2: "))        
 result = addingnum(numa1,numa2)
+print("the result is",end=" ")
 print(result)
