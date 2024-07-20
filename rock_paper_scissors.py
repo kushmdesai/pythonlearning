337641317cf787646a4fb1d4c3e0762b49ec9401
kushmdesai <kushmdesai@icloud.com>
2024-07-20 10:29:35 -0500

    full finishing of the main page

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -20,32 +20,16 @@ smallfont = pygame.font.SysFont('Corbel',35)
 BotText = smallfont.render('bot' , True , color) 
 PlayerText = smallfont.render('player', True, color)
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
-for ev in pygame.event.get(): 
-		
-		if ev.type == pygame.QUIT: 
-			pygame.quit() 
-			
-		#checks if a mouse is clicked 
-		if ev.type == pygame.MOUSEBUTTONDOWN: 
-			mouse = pygame.mouse.get_pos() 
-			#if the mouse is clicked on the 
-			# button the game is terminated 
-			if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40: 
-				pygame.quit() 
 				
-
-while running:
+while running:			
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-			
-    for ev in pygame.event.get():
-        if ev.type == pygame.QUIT: 
-             pygame.quit
-        if ev.type == pygame.MOUSEBUTTONDOWN:
-               if width/2 <= mouse[0] <= width/2+140 and height/2 <= mouse[1] <= height/2+40:
-                      pygame.quit
+        if event.type == pygame.QUIT: 
+             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+               if 240 <= mouse[0] <= 240 + 140 and 340 <= mouse[1] <= 340 + 40:
+                    pygame.quit()
+               if 900 <= mouse[0] <= 900 + 140 and 340 <= mouse[1] <= 340 + 40:
+                    pygame.quit()    
 			
 			#if the mouse is clicked on the 
 			# button the game is terminated 
@@ -53,16 +37,16 @@ while running:
     mouse = pygame.mouse.get_pos()
     screen.blit(BotText , (950,345))
     screen.blit(PlayerText , (270,345))
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
     if 900 <= mouse[0] <= 900 + 140 and 340 <= mouse[1] <= 340 + 40:
         pygame.draw.rect(screen,color_light,[900,340,140,40]) 
     else:
        pygame.draw.rect(screen,color_dark,[900,340,140,40])       
-    if 340 <= mouse[0] <= 240 + 140 and 240 <= mouse[1] <= 340 + 40:
+    if 240 <= mouse[0] <= 240 + 140 and 340 <= mouse[1] <= 340 + 40:
         pygame.draw.rect(screen,color_light,[240,340,140,40])
     else:
           pygame.draw.rect(screen,color_dark,[240,340,140,40])
+    screen.blit(BotText , (950,345))
+    screen.blit(PlayerText , (270,345))
 
 
     # flip() the display to put your work on screen
